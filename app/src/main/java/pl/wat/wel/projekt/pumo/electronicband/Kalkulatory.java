cf28a6b647b9c5cefb321e18e59d8e570087ff81
rev3ng <konsle17@wp.pl>
2017-03-30 00:30:57 +0200

    Logic submenu
    Added logic submenu.
    Added buttons to open logic calcs.

--- app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/Kalkulatory.java
+++ app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/Kalkulatory.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class Kalkulatory extends AppCompatActivity {
     Button prosty;
@@ -17,6 +18,7 @@ public class Kalkulatory extends AppCompatActivity {
 
         prosty = (Button) findViewById(R.id.prosty_k);
         logiczne = (Button) findViewById(R.id.bramki_k);
+
 
 
         prosty.setOnClickListener(new View.OnClickListener() {
@@ -32,8 +34,33 @@ public class Kalkulatory extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                // Intent intent = new Intent (getApplicationContext(), Prosty_Kalkulator.class);
-            }
-        });
+                Button logiczne = (Button) findViewById(R.id.bramki_k);
+                LinearLayout logic_sub = (LinearLayout) findViewById(R.id.logic_submenu);
+                if ( logic_sub.getVisibility() == View.GONE ){
+                    logic_sub.setVisibility(View.VISIBLE);
+                }
+                else {
+                    logic_sub.setVisibility(View.GONE);
+                }
+        }});
 
+    }
+
+    public void onClickNOR(View view) {
+    }
+
+    public void onClickNAND(View view) {
+    }
+
+    public void onClickNOT(View view) {
+    }
+
+    public void onClickAND(View view) {
+    }
+
+    public void onClickOR(View view) {
+    }
+
+    public void onClickXOR(View view) {
     }
 }
