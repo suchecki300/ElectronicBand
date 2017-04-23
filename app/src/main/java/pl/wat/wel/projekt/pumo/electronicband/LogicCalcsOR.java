f5047c9a1e5694fdc5ed64731cd040d9339409a6
Konrad <konsle17@wp.pl>
2017-04-23 22:27:11 +0200

    Calcs ended

--- app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/LogicCalcsOR.java
+++ app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/LogicCalcsOR.java
@@ -17,19 +17,51 @@ public class LogicCalcsOR extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logic_calcs_or);
+        Button aOr = (Button) findViewById(R.id.butAor);
+        Button bOr = (Button) findViewById(R.id.butBor);
+        TextView out = (TextView) findViewById(R.id.outQor);
+        aOr.setText("1");
+        bOr.setText("1");
         Context context = getApplicationContext();
-        CharSequence text = "Dotknij aby ustawic wartosci startowe";
+        CharSequence text = "Dotknij aby zmienic wartosci bramki";
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    public void OrA(View view) {
+        Button aOr = (Button) findViewById(R.id.butAor);
+        Button bOr = (Button) findViewById(R.id.butBor);
+        TextView out = (TextView) findViewById(R.id.outQor);
+
+        if (aOr.getText() == "1"){
+            aOr.setText("0");
+            if (bOr.getText() == "0"){
+                out.setText("0");
+            } else {
+                out.setText("1");
+            }} else {
+            aOr.setText("1");
+            out.setText("1");
+        }
     }
 
     public void OrB(View view) {
         Button aOr = (Button) findViewById(R.id.butAor);
         Button bOr = (Button) findViewById(R.id.butBor);
         TextView out = (TextView) findViewById(R.id.outQor);
+
+        if (bOr.getText() == "1"){
+            bOr.setText("0");
+            if (aOr.getText() == "0"){
+                out.setText("0");
+            } else {
+                out.setText("1");
+            }} else {
+            bOr.setText("1");
+            out.setText("1");
+        }
     }
 
-    public void OrA(View view) {
-    }
+
 }
