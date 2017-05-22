084a55db79261d8a1bcad8850f97cbc6b5e8374d
Damian <chebad@interia.pl>
2017-05-23 00:05:53 +0200

    //Damian created fragments and new layout.

--- app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/DictionarysActivity.java
+++ app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/DictionarysActivity.java
@@ -1,16 +1,12 @@
 package pl.wat.wel.projekt.pumo.electronicband;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-import pl.wat.wel.projekt.pumo.electronicband.Dictionary.BasicTermsActivity;
-import pl.wat.wel.projekt.pumo.electronicband.Dictionary.ChipsActivity;
-import pl.wat.wel.projekt.pumo.electronicband.Dictionary.ElectronicElementsActivity;
-import pl.wat.wel.projekt.pumo.electronicband.Dictionary.WorkshopActivity;
+import pl.wat.wel.projekt.pumo.electronicband.Dictionary.FragmentPagerAdapter;
 
 public class DictionarysActivity extends AppCompatActivity {
 
@@ -24,29 +20,11 @@ public class DictionarysActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dictionarys);
 
-        buttonElectronicElements = (TextView) findViewById(R.id.electronics_elements);
-        buttonChips = (TextView) findViewById(R.id.integrated_circuits);
-        buttonCommonTerms = (TextView) findViewById(R.id.basic_terms);
-        buttonWorkshop = (TextView) findViewById(R.id.workshop);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        FragmentPagerAdapter adapter = new FragmentPagerAdapter(getSupportFragmentManager());
 
-        onClickButton(buttonElectronicElements,ElectronicElementsActivity.class);
-        onClickButton(buttonChips,ChipsActivity.class);
-        onClickButton(buttonCommonTerms,BasicTermsActivity.class);
-        onClickButton(buttonWorkshop,WorkshopActivity.class);
-    }
-
-    /**
-     * Metoda obsługująca przełączanie pomiędzy aktywnościami.
-     * @param button    Przycisk, który ma przełączyć aktywność.
-     * @param c         Aktywność, która się uruchomi po naciśnięciu przycisku.
-     */
-    private void onClickButton(TextView button, final Class c){
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(DictionarysActivity.this,c);
-                startActivity(intent);
-            }
-        });
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/ListsAndAdapters/ListAdapter.java
+++ app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/ListsAndAdapters/ListAdapter.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -59,8 +58,6 @@ public class ListAdapter extends ArrayAdapter<List> {
         ImageView image = (ImageView) listItemView.findViewById(R.id.image);
         tekst.setText(currentList.getWordText());
         image.setImageResource(currentList.getPictureID());
-        //LinearLayout linearLayout = (LinearLayout) listItemView.findViewById(R.id.text_container);
-        //linearLayout.setBackgroundColor(mContext.getResources().getColor(R.color.white));
 
         return listItemView;
     }
