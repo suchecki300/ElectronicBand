e7b54ae96b652f1137ede8ab42d5a056b3986dfc
Damian <chebad@interia.pl>
2017-05-24 09:37:21 +0200

    //Damian Added posibility to write quick mail.

--- app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/AboutUsActivity.java
+++ app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/AboutUsActivity.java
@@ -1,8 +1,12 @@
 package pl.wat.wel.projekt.pumo.electronicband;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -24,6 +28,17 @@ public class AboutUsActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void clickEmail(View view) {
+        String contactEmail = getString(R.string.kontakt_gmail);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:" + contactEmail));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(getApplicationContext(), "Coś poszło nie tak :(", Toast.LENGTH_SHORT).show();
         }
     }
 }
